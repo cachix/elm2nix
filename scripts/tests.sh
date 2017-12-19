ea1391dd88127d30d63febb595236404de68b23f
Robert Vollmert <rob@vllmrt.net>
2017-12-19 15:56:08 +0100

    test that output files are generated

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -p git stack -i bash
 
+set -e -x
+
 stack install --nix
 
 MYTMPDIR=$(mktemp -d)
@@ -9,7 +11,9 @@ trap "rm -rf $MYTMPDIR" EXIT
 git clone https://github.com/debois/elm-mdl.git $MYTMPDIR/elm-mdl
 
 pushd $MYTMPDIR/elm-mdl
-  ~/.local/bin/elm2nix init > default.nix
+  ~/.local/bin/elm2nix init | sed -e '/^  targets/s/\\[\\]/["Material", "Material.Button"]/' > default.nix
   ~/.local/bin/elm2nix convert > elm-srcs.nix
   nix-build
+  test -e ./result/Material.js
+  test -e ./result/Material.Button.js
 popd
