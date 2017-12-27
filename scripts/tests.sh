16c337fa3f601639b90157428ab46dca1c87e490
Robert Vollmert <rob@vllmrt.net>
2017-12-27 18:31:57 +0100

    fix numerous errors in test script

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -p git stack -i bash
 
-set -e -x
+set -e
 
 stack install --nix
 
@@ -10,10 +10,17 @@ trap "rm -rf $MYTMPDIR" EXIT
 
 git clone https://github.com/debois/elm-mdl.git $MYTMPDIR/elm-mdl
 
+checkfile() {
+  if [ ! -e "$1" ]; then
+    echo "file missing: $1"
+    return 1
+  fi
+}
+
 pushd $MYTMPDIR/elm-mdl
-  ~/.local/bin/elm2nix init | sed -e '/^  targets/s/\\[\\]/["Material", "Material.Button"]/' > default.nix
+  ~/.local/bin/elm2nix init | sed -e '/^  targets/s/\[\]/["Material" "Material.Button"]/' > default.nix
   ~/.local/bin/elm2nix convert > elm-srcs.nix
   nix-build
-  test -e ./result/Material.js
-  test -e ./result/Material.Button.js
+  checkfile ./result/Material.js
+  checkfile ./result/Material.Button.js
 popd
