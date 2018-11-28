11aacfd9d670f09c07a40864ebed01fe9e86fe83
Domen Kožar <domen@dev.si>
2018-11-28 17:14:54 +0000

    attempt to fix CI

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/17.09.tar.gz -p git stack -i bash
+#!nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/release-18.09.tar.gz -p git stack -i bash
 
 set -e
 
-NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/17.09.tar.gz stack install --nix
+export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/release-18.09.tar.gz 
+
+stack install --nix
 
 MYTMPDIR=$(mktemp -d)
 trap "rm -rf $MYTMPDIR" EXIT
 
-git clone https://github.com/debois/elm-mdl.git $MYTMPDIR/elm-mdl
+git clone https://github.com/evancz/elm-todomvc.git $MYTMPDIR/elm-todomvc
 
 checkfile() {
   if [ ! -e "$1" ]; then
@@ -17,10 +19,9 @@ checkfile() {
   fi
 }
 
-pushd $MYTMPDIR/elm-mdl
-  ~/.local/bin/elm2nix init | sed -e '/^  targets/s/\[\]/["Material" "Material.Button"]/' > default.nix
+pushd $MYTMPDIR/elm-todomvc
+  ~/.local/bin/elm2nix init > default.nix
   ~/.local/bin/elm2nix convert > elm-srcs.nix
   nix-build
   checkfile ./result/Material.js
-  checkfile ./result/Material.Button.js
 popd
