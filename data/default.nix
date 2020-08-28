9b4e525824e0fd53eafa27e1ae9f775422808f7a
Marek Fajkus <marek.faj@gmail.com>
2020-08-29 00:10:49 +0200

    Fix compatibility with newer versions of uglify.js

--- data/default.nix
+++ data/default.nix
@@ -37,7 +37,7 @@ let
           \${lib.optionalString outputJavaScript ''
             echo "minifying \${elmfile module}"
             uglifyjs $out/\${module}.\${extension} --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \\
-                | uglifyjs --mangle --output=$out/\${module}.min.\${extension}
+                | uglifyjs --mangle --output $out/\${module}.min.\${extension}
           ''}
         '') targets)}
       '';
