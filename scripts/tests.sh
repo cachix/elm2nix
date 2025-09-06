dabb5b2c2134393e4f7c90cad8dc6e2b2653b348
Sander <hey@sandydoo.me>
2025-09-06 12:53:40 +0200

    tests: fix

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-ELM2NIX=$(stack exec which elm2nix)
+WORKDIR=$(mktemp -d)
+trap "rm -rf $WORKDIR" EXIT
 
-git clone https://github.com/evancz/elm-todomvc.git $MYTMPDIR/elm-todomvc
+ELM2NIX=$(cabal exec which elm2nix)
+
+git clone https://github.com/evancz/elm-todomvc.git $WORKDIR/elm-todomvc
 
 checkfile() {
   if [ ! -e "$1" ]; then
@@ -12,7 +15,7 @@ checkfile() {
   fi
 }
 
-pushd $MYTMPDIR/elm-todomvc
+pushd $WORKDIR/elm-todomvc
   $ELM2NIX init > default.nix
   $ELM2NIX convert > elm-srcs.nix
   $ELM2NIX snapshot
